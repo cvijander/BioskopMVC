a012ba84adaf0bb438a632cec86353057edea827
cvijander <cvija85@gmail.com>
2024-10-12 23:06:00 +0200

    Dodavanje CinameHall (sale) i klasa SeatingArea

--- BioskopMVC/Models/CinemaHall.cs
+++ BioskopMVC/Models/CinemaHall.cs
@@ -2,6 +2,27 @@
 {
     public class CinemaHall
     {
+        public int HallId { get; set; }
+        public string HallName { get; set; }
+        public int Capacity { get; set; }
+        public string Type { get; set; }        
+        public List<Movie> Movies { get; set; }
+        public List<SeatingArea> SeatingAreas { get; set; }
 
+        public CinemaHall()
+        {
+            Movies = new List<Movie>();
+            SeatingAreas = new List<SeatingArea>();
+        }
+
+        public CinemaHall(int hallId, string hallName, int capacity, string type, List<Movie> movies, List<SeatingArea> seatingAreas)
+        {
+            HallId = hallId;
+            HallName = hallName;
+            Capacity = capacity;
+            Type = type;
+            Movies = movies;
+            SeatingAreas = seatingAreas;
+        }
     }
 }
---
+++ BioskopMVC/Models/SeatingArea.cs
@@ -0,0 +1,20 @@
+﻿namespace BioskopMVC.Models
+{
+    public class SeatingArea
+    {
+        public int SeatingAreaId { get; set; }
+        public string Sector { get; set; }
+        public string Row { get; set; }
+        public int SeatNumber { get; set; }
+
+        public SeatingArea() { }
+
+        public SeatingArea(int seatingAreaId, string sector, string row, int seatNumber)
+        {
+            SeatingAreaId = seatingAreaId;
+            Sector = sector;
+            Row = row;
+            SeatNumber = seatNumber;
+        }
+    }
+}
