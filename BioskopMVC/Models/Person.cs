6831670979a3e74183ea8bd242c058bbc0d47ffc
cvijander <cvija85@gmail.com>
2024-10-12 19:29:14 +0200

    Person klasa

---
+++ BioskopMVC/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace BioskopMVC.Models
+{
+    public class Person
+    {
+        
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Nationality { get; set; }
+
+        public Person(int id, string firstName, string lastName, DateTime dateOfBirth, string nationality)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Nationality = nationality;
+        }
+
+
+
+    }
+}
