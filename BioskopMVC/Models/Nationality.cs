50932e5c0467e268229c5220f9811b263d099425
cvijander <cvija85@gmail.com>
2024-10-18 13:28:50 +0200

    Klasa Nationality i povezivanje  sa Person klasom

---
+++ BioskopMVC/Models/Nationality.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BioskopMVC.Models
+{
+    public class Nationality
+    {
+        public int NationalityID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }  
+    }
+}
--- BioskopMVC/Models/Person.cs
+++ BioskopMVC/Models/Person.cs
@@ -1,35 +1,42 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BioskopMVC.Models
 {
     public class Person
     {
-        
 
+        [Key]
         public int PersonId { get; set; }
-        
+
+        [Required(ErrorMessage ="Ime je obavezno.")]
+        [StringLength(50, ErrorMessage ="Duzina imena ne moze biti vise od 50 karaktera.")]
         public string FirstName { get; set; }
 
-        
+
+        [Required(ErrorMessage ="Prezime je obavezno.")]
+        [StringLength(50, ErrorMessage ="Duzina prezimena ne moze biti vise od 50 karaktera." )]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage ="Datum rodjenja je obavezan.")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
-        public string Nationality { get; set; }
+        [ForeignKey("Nationality")]
+        public int NationalityId { get; set; }
 
-        public Person(int personId, string firstName, string lastName, DateTime dateOfBirth, string nationality)
+        public Nationality Nationality { get; set; }
+
+        public Person(int personId, string firstName, string lastName, DateTime dateOfBirth, int nationalityId)
         {
             PersonId = personId;
             FirstName = firstName;
             LastName = lastName;
             DateOfBirth = dateOfBirth;
-            Nationality = nationality;
+            NationalityId = nationalityId;
         }
 
         public Person() { }
-
-
-
 
     }
 }
