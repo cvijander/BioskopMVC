fc110a9f939cd7d0d596615f8397bae7297e65fb
cvijander <cvija85@gmail.com>
2024-10-24 17:50:14 +0200

    Tabela Director i MovieActor zbog povezivanje vise na vise tj jedan glumac  na vise fiomva i obrnuto jedan film moze da ima vise glumaca

--- BioskopMVC/Models/Movie.cs
+++ BioskopMVC/Models/Movie.cs
@@ -1,16 +1,44 @@
-﻿namespace BioskopMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BioskopMVC.Models
 {
     public class Movie
     {
+        [Key]
         public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Naslov je obavezan.")]
+        [StringLength(200, ErrorMessage = "Naslov ne moze biti duzi od 200 karaktera.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Duzina trajanja filmova je obavezna.")]
         public TimeSpan Duration { get; set; }
+
+        [Required(ErrorMessage = "Datum izlaska filma je obavezan.")]
+        [DataType(DataType.Date, ErrorMessage = "Unesite ispravan datum.")]
+
         public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Opis filma je obavezan.")]
+        [StringLength(1000, ErrorMessage = "Opis filma ne  moze biti duzi od 1000 karaktera.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Produkcijska kuca je obavezna.")]
+        [StringLength(200, ErrorMessage = "Ime produkcijske kuce ne moze biti duze od 200 karaktera.")]
         public string ProductionCompany { get; set; }
+
+        [Required(ErrorMessage = "Ocena filma je obavezna")]
+        [Range(0.0, 10.0, ErrorMessage = "Ocena mora biti izmedju 0 i 10, sa decimalnim vrednostima")]
         public double Rating { get; set; }
-        public string PosterUrl { get; set; }        
-        public List<Actor> Actors { get; set; }
+
+        [Url(ErrorMessage ="Unesite ispravan URL.")]
+        public string PosterUrl { get; set; }
+
+        [Required(ErrorMessage ="Film mora imati barem jednog glumca.")]
+        public List<Actor> Actors { get; set; } = new List<Actor>();
+
+
+        [Required(ErrorMessage = "Reziser je obavezan.")]
         public Director Director { get; set; }
 
         public bool isActive { get; set; }
